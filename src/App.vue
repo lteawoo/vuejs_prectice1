cd87c07eb1931ef0a3543ec40055bc4ce1eca754
이태우 <twlee@foodtechkorea.com>
2020-08-12 18:45:15 +0900

    menu group

--- src/App.vue
+++ src/App.vue
@@ -11,20 +11,6 @@
     </v-app-bar>
 
     <v-navigation-drawer app v-model="drawer">
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title class="title">
-            Application
-          </v-list-item-title>
-
-          <v-list-item-subtitle>
-            subtext
-          </v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
-
-      <v-divider/>
-
       <site-menu></site-menu>
     </v-navigation-drawer>
 
