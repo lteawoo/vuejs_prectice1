1e340e48e9e4fdc27ccb3f768c96ce6987a4364d
Taewoo Lee <lteawoo@naver.com>
2020-08-13 22:17:26 +0900

    functions

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,28 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+var admin = require('firebase-admin')
+
+var serviceAccount = require('./key.json')
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://taeu-vue-precties1.firebaseio.com'
+})
+
+const db = admin.database()
+
+exports.createUser = functions.auth.user().onCreate(async (user) => {
+  const { uid, email, displayName, photoURL } = user
+  const data = {
+    email,
+    displayName,
+    photoURL,
+    createdAt: new Date()
+  }
+  db.ref('users').child(uid).set(data)
+})
+
+exports.deleteUser = functions.auth.user().onDelete(async (user) => {
+  const { uid } = user
+  db.ref('users').child(uid).remove()
+})
