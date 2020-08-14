3c661e38b6de6b25e884a6a0fc2e2463ec18167a
이태우 <twlee@foodtechkorea.com>
2020-08-14 11:21:09 +0900

    storage

---
+++ src/views/storage.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-sheet>
+    sss
+    <v-btn @click="write">btn</v-btn>
+  </v-sheet>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      text: ''
+    }
+  },
+
+  methods: {
+    async write () {
+      const sn = await this.$firebase.storage().ref().child('xxx.text').putString('abc')
+      const url = await sn.ref.getDownloadURL()
+      console.log(url)
+    }
+  }
+}
+</script>
