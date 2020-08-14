177c21e2d0a7c9d6312ac8f979b782c422f558fb
Taewoo Lee <lteawoo@naver.com>
2020-08-14 21:39:25 +0900

    article update

--- src/views/board/article/form.vue
+++ src/views/board/article/form.vue
@@ -5,14 +5,13 @@
         <v-toolbar-title>게시판 글 작성</v-toolbar-title>
 
         <v-spacer/>
-
-        <v-btn icon @click="read"><v-icon>mdi-allergy</v-icon></v-btn>
         <v-btn icon @click="back"><v-icon>mdi-arrow-left</v-icon></v-btn>
-        <v-btn icon @click="write"><v-icon>mdi-content-save</v-icon></v-btn>
+        <v-btn icon @click="write" v-if="!articleId"><v-icon>mdi-content-save</v-icon></v-btn>
+        <v-btn icon @click="update" v-else><v-icon>mdi-file-document-edit</v-icon></v-btn>
       </v-toolbar>
       <v-card-text>
         <v-text-field v-model="form.title" outlined label="제목"></v-text-field>
-        <editor :initialValue="form.content" ref="editor"></editor>
+        <editor ref="editor"></editor>
       </v-card-text>
     </v-card>
   </v-container>
@@ -21,6 +20,7 @@
 <script>
 export default {
   props: ['document', 'action'],
+
   data () {
     return {
       form: {
@@ -57,8 +57,9 @@ export default {
 
       if (article.exists) {
         const data = article.data()
-
         this.form.title = data.title
+        this.form.content = data.content
+        this.$refs.editor.invoke('setMarkdown', data.content)
       }
     },
 
@@ -67,26 +68,44 @@ export default {
 
       try {
         const now = new Date()
+
         const doc = {
           title: this.form.title,
           updateAt: now,
           content: this.$refs.editor.invoke('getMarkdown')
         }
+
         await this.$firebase.firestore()
           .runTransaction(t => {
             return t.get(this.ref)
               .then(board => {
                 if (board.exists) {
                   const articleCount = board.data().articleCount + 1
+                  doc.createAt = now
 
                   t.update(this.ref, {
                     articleCount: articleCount
                   })
-
                   t.set(this.ref.collection('articles').doc(String(articleCount)), doc)
                 }
               })
           })
+      } finally {
+        this.loading = false
+      }
+    },
+
+    async update () {
+      try {
+        const now = new Date()
+
+        const doc = {
+          title: this.form.title,
+          updateAt: now,
+          content: this.$refs.editor.invoke('getMarkdown')
+        }
+
+        this.$firebase.firestore.update(this.ref.collection('articles').doc(this.articleId), doc)
       } finally {
         this.loading = false
       }
